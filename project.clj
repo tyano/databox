0cf7b578f19366c1dfdbe5a102e809fce1fcb1e9
Tsutomu YANO <t_yano@me.com>
2019-02-08 18:19:28 +0900

    bumpup version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject databox "0.1.1"
+(defproject databox "0.1.2"
   :description "A library for boxing values or exceptions for passing data through channels of core.async."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
