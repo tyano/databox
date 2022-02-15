5aa464125243a12abfbcc7864000978b4e1ce79d
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-02-15 16:34:35 +0900

    0.2.0-SNAPSHOT, clojure 1.10.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject databox "0.1.7"
+(defproject databox "0.2.0-SNAPSHOT"
   :description "A library for boxing values or exceptions for passing data through channels of core.async."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]])
