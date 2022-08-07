00f65f0402b4843a119352b6a8421d3433bbddcf
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-08-07 16:39:28 +0900

    start developing 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject databox "0.2.0"
+(defproject databox "0.2.1-SNAPSHOT"
   :description "A library for boxing values or exceptions for passing data through channels of core.async."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
